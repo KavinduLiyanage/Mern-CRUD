73079b368778eba19dcf5ca71ab9629619cdd84e
Kavindu8956 <sandeepa8956@gmail.com>
2020-05-02 01:15:02 +0530

    Part4 complete

---
+++ src/components/create.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CreateComponent extends Component {
+    render() {
+        return (
+            <div>
+                <p>welcome to create component</p>
+            </div>
+        );
+    }
+}
+
+export default CreateComponent;
---
+++ src/components/edit.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EditComponent extends Component {
+    render() {
+        return (
+            <div>
+                <p>welcome to edit component</p>
+            </div>
+        );
+    }
+}
+
+export default EditComponent;
